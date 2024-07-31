1958336bf8518beb0a09356927134f18bce12274
alejandrocqvd <alejandrocqvd@gmail.com>
2024-07-30 23:03:12 -0600

    user queries

--- backend/main.go
+++ backend/main.go
@@ -3,17 +3,18 @@ package backend
 import (
 	"context"
 	"errors"
+	"time"
 
 	"encore.dev/storage/sqldb"
 )
 
 type User struct {
-	Username  string `json:"username" db:"username"`
-	FirstName string `json:"first_name" db:"first_name"`
-	LastName  string `json:"last_name" db:"last_name"`
-	DOB       string `json:"dob" db:"dob"`
-	Email     string `json:"email" db:"email"`
-	Password  string `json:"password" db:"password"`
+	Username  string    `json:"username" db:"username"`
+	FirstName string    `json:"first_name" db:"first_name"`
+	LastName  string    `json:"last_name" db:"last_name"`
+	DOB       time.Time `json:"dob" db:"dob"`
+	Email     string    `json:"email" db:"email"`
+	Password  string    `json:"password" db:"password"`
 }
 
 type Investment struct {
@@ -48,6 +49,62 @@ func CreateUser(ctx context.Context, p *User) (*User, error) {
 	}
 
 	return p, nil
+}
+
+// encore:api public method=PUT path=/backend
+func UpdateUser(ctx context.Context, p *User) (*User, error) {
+	// Validate
+	if err := ValidateUser(p); err != nil {
+		return nil, err
+	}
+
+	// Update User
+	_, err := db.Exec(ctx,
+		`UPDATE "user"
+		SET first_name = $1, last_name = $2, dob = $3, email = $4, password = $5
+		WHERE username = $6`,
+		p.FirstName, p.LastName, p.DOB, p.Email, p.Password, p.Username)
+	if err != nil {
+		return nil, err
+	}
+
+	return p, nil
+}
+
+// encore:api public method=GET path=/backend/:username
+func GetUser(ctx context.Context, username string) (*User, error) {
+	// Validate
+	if len(username) == 0 {
+		return nil, errors.New("invalid username")
+	}
+
+	// Get User
+	user := &User{Username: username}
+	err := db.QueryRow(ctx,
+		`SELECT first_name, last_name, dob, email, password FROM "user" WHERE username = $1`,
+		username).Scan(&user.FirstName, &user.LastName, &user.DOB, &user.Email, &user.Password)
+	if err != nil {
+		return nil, err
+	}
+
+	return user, nil
+}
+
+// encore:api public method=DELETE path=/backend/:username
+func DeleteUser(ctx context.Context, username string) error {
+	// Validate
+	if len(username) == 0 {
+		return errors.New("invalid username")
+	}
+
+	// Delete User
+	_, err := db.Exec(ctx,
+		`DELETE FROM "user" WHERE username = $1`, username)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func ValidateUser(p *User) error {
